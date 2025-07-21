a9799366293e453e509620c0596c193be79f6379
ntdyx <nataliadiaspp@gmail.com>
2025-07-21 11:12:58 -0300

    config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+// Configurações do projeto
+const CONFIG = {
+    GOOGLE_SHEETS_URL: 'https://script.google.com/macros/s/AKfycbwXYRqmzEwSsj57XD8X9O7EntkA0jJ6NAkqzjXHmlSdHa9kKhchoJFhp8xJyo2N2RHh/exec',
+    APP_NAME: 'Bet Baby',
+    VERSION: '1.0.0',
+    
+    // Configurações de validação
+    VALIDATION: {
+        MIN_WEIGHT: 1,
+        MAX_WEIGHT: 6,
+        MIN_HEIGHT: 30,
+        MAX_HEIGHT: 70
+    },
+    
+    // Configurações de interface
+    UI: {
+        ANIMATION_DURATION: 300,
+        SUCCESS_MESSAGE_DURATION: 2000
+    }
+};
