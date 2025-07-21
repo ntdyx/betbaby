bc86994ed5600b9f1ca8db9f3b6147a2ccffb5cd
ntdyx <nataliadiaspp@gmail.com>
2025-07-21 12:14:28 -0300

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Configuração do Google Sheets
-const GOOGLE_SHEETS_URL = 'https://script.google.com/macros/s/AKfycbwXYRqmzEwSsj57XD8X9O7EntkA0jJ6NAkqzjXHmlSdHa9kKhchoJFhp8xJyo2N2RHh/exec';
+const GOOGLE_SHEETS_URL = 'https://script.google.com/macros/s/AKfycbyamznAjWjvJtz96hUJQ2cjKAYTTtQUbMLb2CDkJhIJAuTKEMI3EvdFppOnxBbQEETq/exec';
 
 // Carregar apostas do localStorage (backup local) ou inicializar array vazio
 let bets = JSON.parse(localStorage.getItem('betBabyData')) || [];
